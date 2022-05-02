fe4224a9bf7df2a08fca9a80f75d1a9e5f67aba0
Lucas Dutra <ldutra98@gmail.com>
2022-05-01 23:08:38 -0300

    Stablish request-broker connection

--- kafka/producer/src/producer/core.py
+++ kafka/producer/src/producer/core.py
@@ -5,7 +5,7 @@ from flask_api import status
 
 from .kafkaconnect import KafkaEventProducer
 
-from .utils import generate_log_message
+from .utils import generate_log_message, Generator
 
 core_router = Blueprint("core", __name__)
 
@@ -13,18 +13,18 @@ logger = logging.getLogger(__file__)
 logger.addHandler(logging.StreamHandler(sys.stdout))
 logger.setLevel(os.getenv("LOG_LEVEL", "INFO"))
 
+TOPIC_NAME = "word-topic"
 
-@core_router.route("/internal-events/", methods=["POST"])
+
+@core_router.route("/word-flow/", methods=["POST"])
 def register_internal_event():
-    logger.debug("REGISTERING INTERNAL EVENT")
+    logger.debug("Request received")
     resultant_data = request.data
 
-    topic_name = resultant_data["topic"]
-    key = resultant_data["key"]
-    event_data = resultant_data["event"]
+    word_qtt = resultant_data["word_qtt"]
 
     logger_message = generate_log_message(
-        "Posting internal event in kafka.", topic_name, resultant_data["event"]
+        "Posting words in event in kafka.", TOPIC_NAME, word_qtt
     )
 
     logger.debug(logger_message)
@@ -34,24 +34,21 @@ def register_internal_event():
             bootstrap_servers=current_app.config["BOOTSTRAP_SERVERS"],
             development_mode=current_app.config["DEVELOPMENT_MODE"],
         )
-        result = producer.send(topic_name, event_data, key)
+        generator = Generator()
+        for _ in range(word_qtt):
+            word = generator.generate_word()
+            result = producer.send(TOPIC_NAME, word)
     except Exception as exc:
         logger.error(
-            "An error occurred while posting internal event.",
-            exc_info=True,
-            extra={
-                "request": request,
-                "data": resultant_data,
-                "topic_name": topic_name,
-            },
+            f"An error occurred while posting words in kafka topic. Error: {exc}"
         )
         return {
-            "message": "Error posting internal event in kafka.",
+            "message": "Error posting words in kafka topic.",
         }, status.HTTP_400_BAD_REQUEST
     else:
-        return result, status.HTTP_201_CREATED
+        return {"status": "ok"}, status.HTTP_201_CREATED
 
 
 @core_router.route("/status/", methods=["GET"])
 def healthcheck():
-    return {"status": "healthy"}, status.HTTP_201_CREATED
+    return {"status": "healthy"}, status.HTTP_200_OK
--- kafka/producer/src/producer/utils.py
+++ kafka/producer/src/producer/utils.py
@@ -1,10 +1,20 @@
 from flask import current_app
+from essential_generators import DocumentGenerator
 
-def generate_log_message(message, topic_name, event):
+
+class Generator:
+    def __init__(self):
+        self.word_generator = DocumentGenerator()
+
+    def generate_word(self) -> str:
+        return self.word_generator.word()
+
+
+def generate_log_message(message, topic_name, quantity) -> str:
     logger_message = (
         f"{message}\n"
         f"Topic name: {topic_name}\n"
         f"Boostrap Servers: {current_app.config['BOOTSTRAP_SERVERS']}\n"
-        f"event: {event}"
+        f"Quantity: {quantity}"
     )
     return logger_message
